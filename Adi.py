55ae1793473efd9d2373765c9805cca919e34507
Adi <kindilhome@gmail.com>
2025-01-01 18:12:49 +0200

    funny joke2

--- Adi.py
+++ Adi.py
@@ -1,3 +1,4 @@
 def funny_joke():
     print("Why do Python programmers prefer dark mode? Because light attracts bugs! 🐍😂")
-funny_joke()
+def funny_joke2():
+    print("Why was the Python function so calm? Because it had all its arguments under control! 🐍😂")
