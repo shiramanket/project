e0799698b4f57e588e287238db4e128837208386
bh-2 <145985624+bh-2@users.noreply.github.com>
2025-01-01 19:16:30 +0200

    return_type_to_get_age

--- yahav.py
+++ yahav.py
@@ -5,7 +5,7 @@ class employe:
         self.year_born = year_born
         self.salary = salary
 
-    def get_age(self):
+    def get_age(self)-> int:
         return dm.now().year - self.year_born
     
     def do_work(self):
